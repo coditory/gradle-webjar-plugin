ab17a4a9bb4d34565e733fd794351a9024944ab6
pmendelski <mendelski.pawel@gmail.com>
2020-04-13 13:14:41 +0200

    Add gradle enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "webjar-plugin"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (!System.getenv("CI").isNullOrEmpty()) {
+            publishAlways()
+            tag("CI")
+        }
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise").version("3.2")
+}
