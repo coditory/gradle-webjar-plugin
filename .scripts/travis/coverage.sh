bdb960f4e8ea234724e3b0728caac25843ccdc99
pmendelski <mendelski.pawel@gmail.com>
2020-04-14 19:12:17 +0200

    Fix test coverage reporting [ci release skip]

--- .scripts/travis/coverage.sh
+++ .scripts/travis/coverage.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
 if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  # Disable TLS 1.3  https://github.com/kt3k/coveralls-gradle-plugin/issues/85
-  ./gradlew jacocoTestReport coveralls -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2"
+  ./gradlew jacocoTestReport coveralls
 else
   echo "Skipping coverage for non master branch"
 fi
